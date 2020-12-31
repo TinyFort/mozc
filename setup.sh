ef8f27c3f820db6ca67388c3c1c56388068efed6
Coooooooozy <coooooooozy@tinyfort.net>
2020-12-31 13:19:17 +0000

    Add download helper.

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+#http://stackoverflow.com/a/246128/5209556
+#https://stackoverflow.com/questions/35006457
+SOURCE="${BASH_SOURCE[0]:-$0}"
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
+
+if [ ! -e .git ]; then
+    git clone \
+        https://github.com/TinyFort/Kurage.git \
+        tmp \
+        --branch prouct \
+        --no-checkout \
+        --depth 1 \
+        --no-single-branch
+    mv --update tmp/.git .
+    rm -rf tmp
+    git reset \
+        --hard \
+        --recurse-submodules \
+        --shallow-submodules \
+        origin/prouct
+    git submodule update --init
+fi
+
+cd docker
+bash setup.sh
