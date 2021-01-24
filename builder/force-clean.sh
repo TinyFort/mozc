3877ff5fcbcde1c8473a0f88dd49b0330cb30c4a
Coooooooozy <coooooooozy@tinyfort.net>
2021-01-24 04:05:55 +0000

    Add build helper scripts.

---
+++ builder/force-clean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+#http://stackoverflow.com/a/246128/5209556
+#https://stackoverflow.com/questions/35006457
+SOURCE="${BASH_SOURCE[0]:-$0}"
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR/../src
+git reset --hard HEAD
+git submodule update
+git clean -fdx
+python2 build_project.py clean
+bazel clean
